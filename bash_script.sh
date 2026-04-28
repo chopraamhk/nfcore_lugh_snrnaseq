7ea6d1de966508ed869fac20e2069bc3bb45ee4c
Mehak Chopra <m.chopra1@universityofgalway.ie>
2026-04-28 17:30:36 +0100

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -1,11 +1,17 @@
-#!/bin/sh 
-#SBATCH --time=07-00:00:00
-#SBATCH --job-name="snrnaseq_nfcore"
+#!/bin/sh
+#SBATCH --job-name="nf-core_snRNAseq"
 #SBATCH -o snRNAseq.o%j
 #SBATCH -e snRNAseq.e%j
 #SBATCH --mail-user=m.chopra1@universityofgalway.ie
 #SBATCH --mail-type=ALL
+#SBATCH --partition="highmem"
+#SBATCH -N 1
+#SBATCH --mem-per-cpu=20G
+#SBATCH --time=120:00:00
 
-module load Anaconda3/2024.02-1
+module load Anaconda3/2024.02-1 
+module load apptainer
 conda activate nfcore
-nextflow run nf-core/scrnaseq -r 4.1.0 -profile singularity --input sample1.csv --outdir results_1_test --genome GRCh38 --protocol 10XV2 --aligner cellranger -c local.config
+
+#code
+nextflow run nf-core/scrnaseq -r 4.1.0 --input samplesheet.csv --outdir results_snrnaseq --genome GRCh38 --protocol 10XV2 --aligner cellranger -profile apptainer -c local.config_google --fasta /data4/mchopra/genewiz_azenta/90-1155124233/analysis_mc/GRCh38.primary_assembly.genome.fa.gz --gtf /data4/mchopra/genewiz_azenta/90-1155124233/analysis_mc/gencode.v44.annotation.gtf.gz -resume
